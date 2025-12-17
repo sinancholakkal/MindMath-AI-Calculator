e8603080dbe84491ea92b8f859007af1ec40a8d5
Abhiram-ks <abhiramks0001@gmial.com>
2025-12-17 11:19:33 +0530

    UI AI testing updates and recognition screen changes

--- lib/core/common/custome_appbar.dart
+++ lib/core/common/custome_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mindmath_ai_calculator/core/colors/app_palette.dart';
+import 'package:mindmath_ai_calculator/core/constant/media_quary.dart';
 import 'package:mindmath_ai_calculator/src/controller/cubit/toggile_cubit/toggle_cubit.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -26,6 +27,34 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       builder: (context, isEnable) {
         return AppBar(
           centerTitle: true,
+          leadingWidth: 120,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              children: [
+                width(context: context, width: 0.02),
+                Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: AppPalette.blue,
+                  size: 20,
+                ),
+                Flexible(
+                  child: Text(
+                    'Calculator',
+                    style: TextStyle(
+                      color: AppPalette.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
           title: isTitle == true
               ? Text(
                   title!,
